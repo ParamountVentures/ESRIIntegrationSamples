26da627382f70c71952ce747899910c18feca7ac
stevenlivz <steven@livz.org>
2019-04-03 14:17:12 +0100

    Demo showing how to add layers dynamically

---
+++ TestEsri/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TestEsri.Models;
+
+namespace TestEsri.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public HomeController(IConfiguration config)
+        {
+            this._config = config;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.AppId = _config.GetValue<string>("EsriAppId");
+
+            var layers = new List<string>();
+            var config_layers = _config.GetSection("Layers").Get<string[]>();
+
+            foreach (string layer in config_layers)
+            {
+                layers.Add(layer);
+            }
+
+            ViewBag.Layers = layers;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
